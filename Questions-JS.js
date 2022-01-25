578ec752f44ebe1ce6f063a0004a18b59302f83b
michael-dwyer <63988347+michael-dwyer@users.noreply.github.com>
2022-01-25 12:16:43 -0800

    Create Questions-JS.js

---
+++ Questions-JS.js
@@ -0,0 +1,39 @@
+const userData = [
+  {
+    id: '111',
+    name: 'Peter',
+    favorites: {
+      food: ['burgers', 'pizza'],
+      activites: ['basketball', "baseball"]
+    },
+  },
+  {
+    id: '222',
+    name: 'John',
+    favorites: {
+      food: ['burgers', 'tacos'],
+      activites: ['football', "golf"]
+    },
+  },
+  {
+    id: '333',
+    name: 'Mary',
+    favorites: {
+      food: ['pizza', 'tacos', 'fried chicken'],
+      activites: ['volleyball', "softball"]
+    },
+  }
+];
+
+// 5. Given the data above, use ".map" to make an array of objects.
+// Each object should have the id of the user and the amount of favorite foods they have.
+// example: [{id: '111', favoriteFoods: 2}]
+
+
+
+// 6. Given the data above, use ".reduce" to make an array of all the names
+// of the people who have pizza as one of their favorite foods.
+// example: ['Peter', 'Mary']
+
+
+// 7. Show us an example of a switch statement being used
